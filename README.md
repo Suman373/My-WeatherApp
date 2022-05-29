1c42f94f72a7c8e464550f14e1ac54510add6285
Suman <iamsuman898@gmail.com>
2022-05-29 15:05:54 +0530

    committing app v1.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# weather-app
+# My WeatherApp
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,216 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="search-box">
+      <input
+        type="text"
+        class="searchbar"
+        placeholder="Search..."
+        v-model="query"
+        @keypress="fetchWeather"
+        @keydown="isVisible=true;"
+
+      />
+      <div class="location-box" v-if="isVisible">
+        <div class="location">{{weather.name}},{{weather.country}}</div>
+        <div class="date">{{weather.localtime}}</div>
+      </div>
+      <div class="normal" v-else>Search a place to see the current weather</div>
+
+      <div class="weather" v-if="isVisible" >
+        <div class="temp">{{Math.round(info.temp_c)}}&deg; C</div>
+        <div class="status">{{info.condition.text}}
+        </div>
+       <div class="iconholder">
+          <img :src="link" alt="icon">
+       </div>
+      </div>
+  </div>
+  <footer>
+    Copyright &copy; my weatherapp Suma373 All Rights reserved 2022
+  </footer>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      isVisible:false,
+      api_key: "18bab2eda6msh0029f8a7d7fcf40p1af132jsn0ef36b5211bd",
+      base_url: "https://weatherapi-com.p.rapidapi.com/current.json",
+      query: '',
+      weather: {},
+      info:{},
+      link:''
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(`${this.base_url}?q=${this.query}`,{
+          method: 'GET',
+          headers:{'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com','X-RapidAPI-Key': '18bab2eda6msh0029f8a7d7fcf40p1af132jsn0ef36b5211bd'
+          }
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results.location;
+      this.info = results.current;
+      this.link = this.info.condition.icon;
+    },
+  },
+};
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html {
+  overflow-x: hidden;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+body {
+  height: 100vh;
+  width: 100vw;
+}
+#app{
+  height: 100%;
+  width: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  background: url("./assets/cold-bg.jpg") no-repeat center center/cover;
+  animation:weather 15s ease-in-out infinite;
+  transition: animation 600ms;
+}
+@keyframes weather{
+  33%{
+    background:url('./assets/warm-bg.jpg') no-repeat center/cover;
+  }
+  66%{
+    background:url('./assets/rainy-bg.jpg') no-repeat center/cover;
+  }
+}
+
+.normal{
+  font-size:1.5rem;
+  padding:10px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top:5rem;
+}
+.search-box {
+  height: 25rem;
+  width: 25rem;
+  color: white;
+  border-radius: 20px;
+  background: rgba(19, 19, 51, 0.568);
+  box-shadow: 0 0 3px black;
+}
+.searchbar {
+  height: 3rem;
+  font-size: 1.2rem;
+  margin-top: 1rem;
+  width: 100%;
+  background: transparent;
+  color: white;
+  padding: 10px;
+  border: none;
+  border-bottom: 2px solid black;
+  outline: none;
+}
+input::placeholder {
+  color: white;
+}
+
+/* location info container */
+.location-box {
+  height: 6rem;
+  padding: 10px;
+  text-align: center;
+  color: white;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.301);
+}
+.location {
+  font-size: 1.5rem;
+   color:yellow;
+  font-weight: 400;
+  text-shadow: 0 0 2px black;
+}
+
+.date {
+  font-size: 1.2rem;
+}
+.weather {
+  height: 8rem;
+  width: 12rem;
+  margin: 1rem auto;
+  background: rgba(0, 0, 0, 0.301);
+  box-shadow: 0 0 3px white;
+  border-radius: 10px;
+  text-align: center;
+}
+.temp {
+  font-size: 3.5rem;
+  font-weight:800;
+  font-style: italic;
+}
+.status {
+  height:3rem;
+  width:100%;
+  color:yellow;
+  font-size: 1.2rem;
+}
+.iconholder{
+  margin-top:10px;
+  text-align: center;
+}
+footer{
+  width:100%;
+  height:2rem;
+  background: rgba(0, 0, 0, 0.322);
+  position: fixed;
+  bottom:0;
+  font-size:1.1rem;
+  color:yellow;
+  text-align: center;
+}
+
+@media screen and (max-width:460px){
+  #app{
+    height:100vh;
+    width:100vw;
+  }
+ .search-box{
+   width:20rem;
+ }
+ .weather{
+   width:10rem;
+ }
+ .temp{
+   font-size:2.8rem;
+ }
+ .status{
+   font-size:1rem;
+ }
+ .iconholder{
+   margin-top:15px;
+ }
+ footer{
+   font-size:1rem;
+ }
 }
 </style>
